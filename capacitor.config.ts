b27f0b07c17f323aaf11ca0f5fba9be3d5e6c4ae
skulldewdev-a11y <skulldewdev@gmail.com>
2025-12-01 21:41:07 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pinedash.obd',
+  appName: 'Pine Dash',
+  webDir: 'www'
+};
+
+export default config;
